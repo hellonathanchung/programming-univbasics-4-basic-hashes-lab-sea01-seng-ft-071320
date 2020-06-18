ef40e3edef1a1f1e36419ce87348ed45a4cb2340
Nathan Chung <chung.nathanh@gmail.com>
2020-06-18 01:27:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,10 +7,10 @@ def my_hash
 end
 
 def pioneer
-  { :name => "Grass Hopper"
+  { :name => "Grass Hopper"}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {  :id => number}
 end
